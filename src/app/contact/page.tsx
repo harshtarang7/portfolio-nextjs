ed19b94668b8f56dc9e73229e5d3870567ea9dfa
harshtarang7 <tarangharsh7@gmail.com>
2025-08-20 20:17:35 +0530

    added contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { Grid } from "@mui/material"
+
+const Contact = ()=>{
+    return(
+        <>
+        <Grid container>
+            this is contact
+        </Grid>
+        </>
+    )
+}
